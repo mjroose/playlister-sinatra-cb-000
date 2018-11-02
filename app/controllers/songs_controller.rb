3c5fa909fb4762aad1e4d470d6bc05d2ebdc5de1
Michael Roose <mjroose@gmail.com>
2018-11-02 00:58:28 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -72,5 +72,15 @@ class SongsController < Sinatra::Base
 
   patch '/songs/:slug' do
     binding.pry
+    @song = Song.find_by_slug(params[:slug])
+    song_name = params["song"]["name"]
+    artist_name = params["artist"]["name"]
+    
+    if song_name
+      @song.name = song_name
+      artist = Artist.find_or_create_by(name: params["artist"]["name"])
+      if artist
+        @song.artist = artist
+      end
   end
 end
